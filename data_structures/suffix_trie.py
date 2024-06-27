3e835b100b6135c58ddfacf4f28723c9f7a92fcc
vic-bart <vicbart08@gmail.com>
2024-06-27 18:30:31 +1000

    Improved visual appeal of suffix trie string representation by combining columns that only contained branched terminal characters.

--- data_structures/suffix_trie.py
+++ data_structures/suffix_trie.py
@@ -66,17 +66,50 @@ class SuffixTrie():
     column:list[int] = [0]
     self._str_aux(array, stack, depth, column)
     string:str = ""
-    for sub_array in array:
-      for character in sub_array:
-        string += character + " "
+    for i in range(len(array)):
+      try:
+        j = array[i].index("\\")
+        if array[i+1][j] == "$":
+          while array[i][j-1] == " ":
+            value = array[i][j-1]
+            array[i][j-1] = array[i][j]
+            array[i][j] = value
+            value = array[i+1][j-1]
+            array[i+1][j-1] = array[i+1][j]
+            array[i+1][j] = value
+            j -= 1
+      except ValueError:
+        pass
+
+    empty_column = True
+    while empty_column:
+      for j in range(len(array[1])):
+        empty_column = True
+        for i in range(1, len(array)):
+          try:
+            if array[i][j] != " ":
+              empty_column = False
+              break
+          except IndexError:
+            break
+        if empty_column:
+          for i in range(1, len(array)):
+            try:
+              del array[i][j]
+            except IndexError:
+              break
+
+    for i in range(len(array)):
+      for j in range(len(array[i])):
+        string += array[i][j] + "   "
       string += "\n"
     return string
   
   def _str_aux(self, array:list[list[str]], stack:list[SuffixTrieNode], depth:int, column:list[int]):
     for i in range(len(stack[-1].children)):
-      if i > 0:
-        column[0] += 1
       child = stack[-1].children[i]
+      if (i > 0):
+        column[0] += 1
       while (len(array[depth-1])) < column[0]:
         array[depth-1].append(" ")
       while (len(array[depth])) < column[0]:
@@ -93,6 +126,6 @@ class SuffixTrie():
 
 if __name__ == "__main__":
   suffix_trie:SuffixTrie = SuffixTrie()
-  string:str = "ABCABCABCABC"
+  string:str = "BANANANA"
   suffix_trie.insert(string)
   print(suffix_trie)
