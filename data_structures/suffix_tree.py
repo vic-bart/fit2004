e13e621ffcfa787077f28ee25a0d49e75ed2ef39
vic-bart <vicbart08@gmail.com>
2024-06-28 21:51:36 +1000

    Cleaned up suffix trie code by isolating suffix trie generation to the SuffixTrie class, and removing recursive insert function for SuffixTrieNode.

--- data_structures/suffix_tree.py
+++ data_structures/suffix_tree.py
@@ -1,16 +1,47 @@
 """
 """
 
+from typing import TypeVar
+
+SuffixTreeNode = TypeVar("SuffixTreeNode")
+
 class SuffixTreeNode():
-  def __init__(self):
+
+  def __init__(self) -> None:
+    pass
+
+  def set_character(self, character:str) -> None:
+    pass
+
+  def get_character(self) -> str:
+    pass
+
+  def set_child(self, character:str) -> None:
+    pass
+
+  def get_child(self, character:str) -> SuffixTreeNode:
+    pass
+  
+  def has_child(self, character:str) -> bool:
+    pass
+  
+  def has_children(self) -> bool:
+    pass
+  
+  def insert(self, suffix:str):
     pass
 
 class SuffixTree():
+
   def __init__(self):
+    pass
+
+  def insert(self, string:str) -> None:
     pass
 
 
 if __name__ == "__main__":
-  suffix_tree:SuffixTree = SuffixTree()
+  suffix_trie:SuffixTree = SuffixTree()
   string:str = "ABCABCABC"
-  suffix_tree.insert(string)
+  suffix_trie.insert(string)
+  print(suffix_trie)
--- data_structures/suffix_trie.py
+++ data_structures/suffix_trie.py
@@ -1,6 +1,5 @@
 """
 """
-
 from typing import TypeVar
 
 SuffixTrieNode = TypeVar("SuffixTrieNode")
@@ -36,13 +35,6 @@ class SuffixTrieNode():
   
   def has_children(self) -> bool:
     return (len(self.children) > 0)
-  
-  def insert(self, suffix:str):
-    if not self.has_child(suffix[0]):
-      self.set_child(suffix[0])
-    if len(suffix) > 1:
-      node = self.get_child(suffix[0])
-      node.insert(suffix[1:])
 
 class SuffixTrie():
 
@@ -50,13 +42,23 @@ class SuffixTrie():
     self.terminal_character:str = "$"
 
   def insert(self, string:str) -> None:
-    self.string = string
+    """
+    Populates a suffix trie.
+    """
+    self.string = string + self.terminal_character
     self.root = SuffixTrieNode()
 
-    for i in range(len(string)):
-      self.root.insert(string[i:] + self.terminal_character)
+    for i in range(len(self.string)):
 
-    self.root.insert(self.terminal_character)
+      node:SuffixTrieNode = self.root
+
+      for j in range(i, len(self.string)):
+
+        if not node.has_child(self.string[j]):
+
+          node.set_child(self.string[j])
+
+        node = node.get_child(self.string[j])
 
   def __str__(self) -> str:
     array:list[list[str]] = [[] for _ in range((2 * (len(self.string)+1)) + 1)]
@@ -101,7 +103,7 @@ class SuffixTrie():
 
     for i in range(len(array)):
       for j in range(len(array[i])):
-        string += array[i][j] + "   "
+        string += array[i][j] + " "
       string += "\n"
     return string
   
@@ -126,6 +128,6 @@ class SuffixTrie():
 
 if __name__ == "__main__":
   suffix_trie:SuffixTrie = SuffixTrie()
-  string:str = "BANANANA"
+  string:str = "AABBAABB"
   suffix_trie.insert(string)
   print(suffix_trie)
