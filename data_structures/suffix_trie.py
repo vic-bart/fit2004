e69b8e5d2734bd0223b42792eea29dd517447830
vic-bart <vicbart08@gmail.com>
2024-07-02 11:11:02 +1000

    Cleaned up suffix trie file

--- data_structures/suffix_trie.py
+++ data_structures/suffix_trie.py
@@ -65,91 +65,7 @@ class SuffixTrie():
         node = node.get_child(self.string[j])
 
   def __str__(self) -> str:
-    array:list[list[str]] = [[] for _ in range((2 * (len(self.string)+1)) + 1)]
-    array[0].append("root")
-    stack:list[SuffixTrieNode] = [self.root]
-    depth:int = 2
-    column:list[int] = [0]
-    self._str_aux(array, stack, depth, column)
-    string:str = ""
-    for i in range(len(array)):
-      try:
-        j = array[i].index("\\")
-        if array[i+1][j] == "$":
-          while array[i][j-1] == " ":
-            value = array[i][j-1]
-            array[i][j-1] = array[i][j]
-            array[i][j] = value
-            value = array[i+1][j-1]
-            array[i+1][j-1] = array[i+1][j]
-            array[i+1][j] = value
-            j -= 1
-      except ValueError:
-        pass
-
-    empty_column = True
-    while empty_column:
-      for j in range(len(array[1])):
-        empty_column = True
-        for i in range(1, len(array)):
-          try:
-            if array[i][j] != " ":
-              empty_column = False
-              break
-          except IndexError:
-            break
-        if empty_column:
-          for i in range(1, len(array)):
-            try:
-              del array[i][j]
-            except IndexError:
-              break
-
-    for i in range(len(array)):
-      for j in range(len(array[i])):
-        string += array[i][j] + " "
-      string += "\n"
-    return string
   
-  def _str_aux(self, array:list[list[str]], stack:list[SuffixTrieNode], depth:int, column:list[int]):
-    for i in range(len(stack[-1].children)):
-      child = stack[-1].children[i]
-      if (i > 0):
-        column[0] += 1
-      while (len(array[depth-1])) < column[0]:
-        array[depth-1].append(" ")
-      while (len(array[depth])) < column[0]:
-        array[depth].append(" ")
-      if i == 0:
-        array[depth-1].append("|")
-      else:
-        array[depth-1].append("\\")
-      array[depth].append(child.get_character())
-      stack.append(child)
-      self._str_aux(array, stack, depth+2, column)
-      stack.pop()
-
-  def temp1(self, stack, current_depth, previous_depth, array, spacing):
-    for i in range(len(stack[-1].children)):
-      child = stack[-1].children[i]
-      stack.append(child)
-      previous_depth = self.temp1(stack, current_depth+1, previous_depth, array, spacing)
-    node = stack.pop()
-    print(node.get_character(), previous_depth - current_depth)
-    previous_depth = current_depth
-    array[current_depth*2].append(node.get_character())
-    return previous_depth
-
-  def temp2(self, array, depth=0):
-    print('')
-    for value in array:
-      if type(value) == str:
-        print(f"{' ' * depth}{value}")
-      else:
-        self.temp2(value, depth+1)
-
-  def temp3(self):   
-
     table = [[] for _ in range(len(self.string) * 2)]
     row = 1
     targets = [3]
@@ -231,14 +147,4 @@ if __name__ == "__main__":
   suffix_trie:SuffixTrie = SuffixTrie()
   string:str = "ABCDEFGHIJKLMNOPQRSTUVWXYZAABBCCDDEEFFGGSHDNWOANDOAWEFEF"
   suffix_trie.insert(string)
-  print(suffix_trie.temp3())
-  exit(0)
-  stack = [suffix_trie.root]
-  current_depth = 0
-  previous_depth = len(suffix_trie.string)+1
-  array = [[] for _ in range((len(suffix_trie.string)*2)+1)]
-  spacing = ' ' * 1
-  suffix_trie.temp1(stack, current_depth, previous_depth, array, spacing)
-  for subarray in array:
-    print(subarray)
-  # print(suffix_trie)
+  print(suffix_trie)
